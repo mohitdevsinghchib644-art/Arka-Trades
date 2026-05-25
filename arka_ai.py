3880c05c05eeba8cfb95affb9e66c521c5cd66df
mohitdevsinghchib644-art <mohitdevsinghchib644@gmail.com>
2026-05-25 21:23:30 +0530

    Update arka_ai.py

--- arka_ai.py
+++ arka_ai.py
@@ -112,7 +112,7 @@ def get_pinecone_index():
         existing = [i.name for i in pc.list_indexes()]
         if INDEX_NAME not in existing:
             pc.create_index(
-                name=INDEX_NAME, dimension=768, metric="cosine",
+                name=INDEX_NAME, dimension=3072, metric="cosine",
                 spec=ServerlessSpec(cloud="aws", region="us-east-1")
             )
             time.sleep(3)
