de2b372523a66383df3f94efe9390963a7535604
mohitdevsinghchib644-art <mohitdevsinghchib644@gmail.com>
2026-07-25 22:48:23 +0530

    Update breadth_ai.py

--- breadth_ai.py
+++ breadth_ai.py
@@ -154,6 +154,8 @@ def get_hmm_regime(history: pd.DataFrame) -> Optional[dict]:
         }
     except Exception:
         return None
+
+
 def generate_breadth_ai_narrative(*args, **kwargs):
     """Bridge wrapper for breadth_page.py compatibility."""
     return generate_ai_narrative(*args, **kwargs)
